38dfe1964b01161ce99ab5c3483e666a3d3ec5f8
manuelfhinojosa <ingmanuelhinojosa@gmail.com>
2025-11-16 19:37:04 -0500

    task 3 update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,8 +40,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const author = req.params.author;
-    res.send(books[author]);
+  const targetauthor = req.params.author;
+  let book = books.find(book => book.author === targetauthor);
+
+    res.send(books[book.isbn]);
 });
 
 // Get all books based on title
