e4f0c0e29538bc5dfab273fa10e310d2f8f9ac80
manuelfhinojosa <ingmanuelhinojosa@gmail.com>
2025-11-20 23:32:40 -0500

    task 13 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,27 +25,27 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-/*public_users.get('/',function (req, res) {
+public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4));
-});*/
+});
 
 // Get the book list available in the shop
-public_users.get('/', function(req,res){
+/*public_users.get('/', function(req,res){
     Promise.resolve(books).then((data)=>{
         res.send(JSON.stringify(data,null,4));
     }).catch((err)=>{
         res.status(500).send({error:'Something went wrong', details: err});
     });
-});
+});*/
 
 // Get book details based on ISBN
-/*public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
     res.send(books[isbn]);
- });*/
+ });
 
  // Get book details based on ISBN
- public_users.get('/isbn/:isbn', function(req, res){
+ /*public_users.get('/isbn/:isbn', function(req, res){
     const isbn = req.params.isbn;
     new Promise((resolve, reject)=> {
         const book = books[isbn];
@@ -59,7 +59,7 @@ public_users.get('/', function(req,res){
     }).catch((err)=>{
         res.status(404).send({error: err});
     });
- });
+ });*/
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -71,6 +71,26 @@ public_users.get('/author/:author',function (req, res) {
     res.send(targetbook);
 });
 
+// Get book details based on author
+/*public_users.get('/author/:author', function(req, res){
+    const targetauthor = req.params.author;
+
+    new Promise((resolve, reject)=> {
+        const allbooks = Object.values(books);
+        const targetbook = allbooks.filter(book => book.author === targetauthor);
+
+        if(targetbook.length > 0){
+            resolve(targetbook);
+        } else{
+            reject("No books found for this author");
+        }
+    }).then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        res.status(404).send({error: err});
+    });
+});*/
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   
@@ -80,6 +100,26 @@ public_users.get('/title/:title',function (req, res) {
 
     res.send(targetbook);
 });
+
+// Get all books based on title
+/*public_users.get('/title/:title', function(req, res){
+    const targettitle = req.params.title;
+
+    new Promise((resolve, reject)=> {
+        const allbooks = Object.values(books);
+        const targetbook = allbooks.filter(book => book.title === targettitle);
+
+        if(targetbook.length > 0){
+            resolve(targetbook);
+        } else{
+            reject("No books found with this title");
+        }
+    }).then((books)=>{
+        res.send(books);
+    }).catch((err)=>{
+        res.status(404).send({error: err});
+    });
+});*/
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
