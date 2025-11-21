76e92611d7254f0a7b459beaec55b4626fd3d461
manuelfhinojosa <ingmanuelhinojosa@gmail.com>
2025-11-20 22:22:15 -0500

    task11 in progress

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,17 +25,27 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
+/*public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4));
+});*/
+
+// Get the book list available in the shop
+public_users.get('/', function(req,res){
+    Promise.resolve(books).then((data)=>{
+        res.send(JSON.stringify(data,null,4));
+    }).catch((err)=>{
+        res.status(500).send({error:'Something went wrong', details: err});
+    });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+/*public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
     res.send(books[isbn]);
- });
+ });*/
+
+ // Get book details based on ISBN
+ public_users.get('/isbn/:isbn')
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
