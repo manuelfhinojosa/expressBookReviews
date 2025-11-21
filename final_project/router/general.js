98d6ab281c27232b1e562c85b18d0f2ebea0a0f9
manuelfhinojosa <ingmanuelhinojosa@gmail.com>
2025-11-20 23:07:55 -0500

    task 11 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,7 +45,21 @@ public_users.get('/', function(req,res){
  });*/
 
  // Get book details based on ISBN
- public_users.get('/isbn/:isbn')
+ public_users.get('/isbn/:isbn', function(req, res){
+    const isbn = req.params.isbn;
+    new Promise((resolve, reject)=> {
+        const book = books[isbn];
+        if(book){
+            resolve(book);
+        } else{
+            reject("Book not found");
+        }
+    }).then((book)=>{
+        res.send(book);
+    }).catch((err)=>{
+        res.status(404).send({error: err});
+    });
+ });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
